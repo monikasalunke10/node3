6fa7125d57f829949e921f07f43f8a0de70d35e4
aarushtechnologies <aarushtechnologies7@gmail.com>
2026-01-21 14:05:04 +0530

    Done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+// Jenkins / PM2 dynamic port
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Unable to read JSON file' });
+    }
+
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
